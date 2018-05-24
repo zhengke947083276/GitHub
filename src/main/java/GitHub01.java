aafc5d710657c366c1f48b04141e3774b90fb325
94708 <947083276@qq.com>
2018-05-24 20:51:56 +0800

    添加方法

--- src/main/java/GitHub01.java
+++ src/main/java/GitHub01.java
@@ -2,4 +2,7 @@ public class GitHub01 {
     public static void main(String[] args) {
         System.out.println("GitHub测试！！！");
     }
+    public void T(){
+        System.out.println("成功！！！");
+    }
 }
